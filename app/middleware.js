a18682750e908eaae5e7f0397e5e7adc56f43417
flch0707 <florian.cheveau@proton.me>
2024-11-03 15:42:47 +0100

    validation middleware

--- app/middleware.js
+++ app/middleware.js
@@ -1,12 +1,24 @@
+const { validationResult } = require("express-validator");
+
 const handleNoUserData = (res, userData) => {
-    if (!userData) {
-        res.statusCode = 404
-        return res.json('can not get user')
-    }
+  if (!userData) {
+    res.statusCode = 404;
+    return res.json("can not get user");
+  }
 
-    return res.json({data: userData})
-}
+  return res.json({ data: userData });
+};
 
-module.exports = {
-    handleNoUserData
-}
+const validateRequest = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      status: "error",
+      message: "Invalid parameters",
+      errors: errors.array(),
+    });
+  }
+  next();
+};
+
+module.exports = { handleNoUserData, validateRequest };
--- app/routes.js
+++ app/routes.js
@@ -1,50 +1,50 @@
-const express = require('express')
-const idx = require('idx')
-
-const router = express.Router()
-
-const {
-    getUserById,
-    getUserActivityById,
-    getUserAverageSession,
-    getUserPerformance
-} = require('./models')
+const express = require("express");
+const idx = require("idx");
+const { check, validationResult } = require("express-validator");
+const router = express.Router();
 
 const {
-    handleNoUserData
-} = require('./middleware')
+  getUserById,
+  getUserActivityById,
+  getUserAverageSession,
+  getUserPerformance,
+} = require("./models");
 
+const { handleNoUserData, validateRequest } = require("./middleware");
 
-router.get('/user/:id', (req, res) => {
-    const userId = idx(req, _ => _.params.id)
-    const userData = getUserById(Number(userId))
+const delay = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
-    return handleNoUserData(res, userData)
-})
+router.get(
+  "/user/:id",
+  [
+    check("id").isInt().withMessage("User ID must be an integer"),
+    validateRequest,
+  ],
+  (req, res) => {
+    const userId = idx(req, (_) => _.params.id);
+    const userData = getUserById(Number(userId));
+    return handleNoUserData(res, userData);
+  }
+);
 
+router.get("/user/:id/activity", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserActivityById(Number(userId));
+  return handleNoUserData(res, userData);
+});
 
-router.get('/user/:id/activity', (req, res) => {
-    const userId = idx(req, _ => _.params.id)
-    const userData = getUserActivityById(Number(userId))
+router.get("/user/:id/average-sessions", async (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserAverageSession(Number(userId));
+  await delay(3000);
+  return handleNoUserData(res, userData);
+});
 
-    return handleNoUserData(res, userData)
-})
+router.get("/user/:id/performance", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserPerformance(Number(userId));
 
+  return handleNoUserData(res, userData);
+});
 
-router.get('/user/:id/average-sessions', (req, res) => {
-    const userId = idx(req, _ => _.params.id)
-    const userData = getUserAverageSession(Number(userId))
-
-    return handleNoUserData(res, userData)
-})
-
-
-router.get('/user/:id/performance', (req, res) => {
-    const userId = idx(req, _ => _.params.id)
-    const userData = getUserPerformance(Number(userId))
-
-    return handleNoUserData(res, userData)
-})
-
-
-module.exports = router
+module.exports = router;
